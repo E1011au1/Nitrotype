ef63822fb7e87aa99166fbf7efa6f9defddf51dc
E1011au1 <eason.paz@gmail.com>
2025-05-21 15:47:34 +1000

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,1 +1,14 @@
-javascript:(function(){    alert("Full credits to eason. ⚠️⚠️⚠️ I AM NOT RESPONSIBLE FOR YOUR ACC GETTING BANNED ⚠️⚠️⚠️");      var win,        accuracy = 80,        t = Object.values(document.querySelector("div.dash-copyContainer"))[1].children._owner.stateNode;    win = function(r) {        t.handleKeyPress(            "character",            new KeyboardEvent("keypress", { key: r ? t.props.lessonContent[t.typedIndex] : "$" })        );    };    var m = t.input.keyHandler;    t.input.keyHandler = function(e, n) {        if (n.key === "\n") {            t.props.useNitro();            return;        }        if (e === "character") {            win(Math.random() < (accuracy / 80));        }    };})();
+javascript:(function(){    alert("Full credits to eason. ⚠️⚠️⚠️ I AM NOT RESPONSIBLE FOR YOUR ACC GETTING BANNED ⚠️⚠️⚠️
+
+                                 
+ /$$   /$$ /$$   /$$                      /$$$$$$$$                                  /$$   /$$                     /$$             /$$    /$$  /$$$$$$ 
+| $$$ | $$|__/  | $$                     |__  $$__/                                 | $$  | $$                    | $$            | $$   | $$ /$$__  $$
+| $$$$| $$ /$$ /$$$$$$    /$$$$$$   /$$$$$$ | $$ /$$   /$$  /$$$$$$   /$$$$$$       | $$  | $$  /$$$$$$   /$$$$$$$| $$   /$$      | $$   | $$|__/  \ $$
+| $$ $$ $$| $$|_  $$_/   /$$__  $$ /$$__  $$| $$| $$  | $$ /$$__  $$ /$$__  $$      | $$$$$$$$ |____  $$ /$$_____/| $$  /$$/      |  $$ / $$/  /$$$$$$/
+| $$  $$$$| $$  | $$    | $$  \__/| $$  \ $$| $$| $$  | $$| $$  \ $$| $$$$$$$$      | $$__  $$  /$$$$$$$| $$      | $$$$$$/        \  $$ $$/  /$$____/ 
+| $$\  $$$| $$  | $$ /$$| $$      | $$  | $$| $$| $$  | $$| $$  | $$| $$_____/      | $$  | $$ /$$__  $$| $$      | $$_  $$         \  $$$/  | $$      
+| $$ \  $$| $$  |  $$$$/| $$      |  $$$$$$/| $$|  $$$$$$$| $$$$$$$/|  $$$$$$$      | $$  | $$|  $$$$$$$|  $$$$$$$| $$ \  $$         \  $/   | $$$$$$$$
+|__/  \__/|__/   \___/  |__/       \______/ |__/ \____  $$| $$____/  \_______/      |__/  |__/ \_______/ \_______/|__/  \__/          \_/    |________/
+                                                 /$$  | $$| $$                                                                                         
+                                                |  $$$$$$/| $$                                                                                         
+                                                 \______/ |__/");      var win,        accuracy = 80,        t = Object.values(document.querySelector("div.dash-copyContainer"))[1].children._owner.stateNode;    win = function(r) {        t.handleKeyPress(            "character",            new KeyboardEvent("keypress", { key: r ? t.props.lessonContent[t.typedIndex] : "$" })        );    };    var m = t.input.keyHandler;    t.input.keyHandler = function(e, n) {        if (n.key === "\n") {            t.props.useNitro();            return;        }        if (e === "character") {            win(Math.random() < (accuracy / 80));        }    };})();
